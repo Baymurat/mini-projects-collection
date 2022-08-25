dd469785a43af2f768ced2479ad9401cabbb7415
Baymurat <baymurat.orazov@mail.ru>
2022-08-25 19:45:46 +0500

    add project template generator

---
+++ generator/index.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const projectName = process.argv[2]
+console.log(projectName);
+
+if(!fs.existsSync(projectName)) {
+  fs.mkdirSync(`${projectName}/src`, { recursive: true });
+  fs.mkdirSync(`${projectName}/style`, { recursive: true });
+  fs.mkdirSync(`${projectName}/template`, { recursive: true });
+  fs.writeFileSync(`${projectName}/src/index.ts`, '');
+  fs.writeFileSync(`${projectName}/style/style.scss`, '');
+  fs.writeFileSync(`${projectName}/template/index.html`, '');
+  fs.writeFileSync(`${projectName}/tsconfig.json`, '{\n  "include": ["src/**/*"]\n  }');
+}
