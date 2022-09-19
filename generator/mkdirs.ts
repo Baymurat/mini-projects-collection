8c5a05cfb9f7bd0eb52d11611badbeead084771a
Baymurat <baymurat.orazov@mail.ru>
2022-09-19 16:25:20 +0500

    generate react template

--- generator/mkdirs.ts
+++ generator/mkdirs.ts
@@ -1,13 +1,28 @@
 import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const projectName = process.argv[2];
+const isReactTemplate = process.argv[3];
+
 
 if (!fs.existsSync(projectName)) {
   fs.mkdirSync(`${projectName}/src`, { recursive: true });
   fs.mkdirSync(`${projectName}/style`, { recursive: true });
   fs.mkdirSync(`${projectName}/template`, { recursive: true });
-  fs.writeFileSync(`${projectName}/src/index.ts`, "");
+
+  if (isReactTemplate === "true") {
+    fs.copyFileSync(`${__dirname}/template/react/index.tsx`, `${projectName}/src/index.tsx`);
+    fs.copyFileSync(`${__dirname}/template/react/index.html`, `${projectName}/template/index.html`);
+    fs.copyFileSync(`${__dirname}/template/react/App.tsx`, `${projectName}/src/App.tsx`);
+    fs.copyFileSync(`${__dirname}/template/react/tsconfig.json`, `${projectName}/tsconfig.json`);
+  } else {
+    fs.writeFileSync(`${projectName}/src/index.ts`, "");
+    fs.writeFileSync(`${projectName}/tsconfig.json`, "{\n  \"include\": [\"src/**/*\"]\n  }");
+    fs.writeFileSync(`${projectName}/template/index.html`, "");
+  }
   fs.writeFileSync(`${projectName}/style/style.scss`, "");
-  fs.writeFileSync(`${projectName}/template/index.html`, "");
-  fs.writeFileSync(`${projectName}/tsconfig.json`, "{\n  \"include\": [\"src/**/*\"]\n  }");
 }
--- generator/modify.ts
+++ generator/modify.ts
@@ -1,12 +1,14 @@
 import fs from "fs";
 
 const projectName = process.argv[2];
+const isReactTemplate = process.argv[3] === "true";
 
 if (fs.existsSync(`${projectName}/package.json`)) {
   const file = fs.readFileSync(`${projectName}/package.json`, "utf-8");
   const testScript = "\"test\": \"echo \\\"Error: no test specified\\\" && exit 1\"";
+  const react = isReactTemplate ? "--env index='./src/index.tsx'" : "";
   // eslint-disable-next-line
-  const startScript = `"start": "webpack-dev-server --config ../webpack.config.js --env base=${projectName} --env title=\'${projectName.toUpperCase()}\'"`;
+  const startScript = `"start": "webpack-dev-server --config ../webpack.config.js ${react} --env base=${projectName} --env title=\'${projectName.toUpperCase()}\'"`;
   const writeFile = file.replace(testScript, startScript);
   fs.writeFileSync(`${projectName}/package.json`, writeFile);
 }
