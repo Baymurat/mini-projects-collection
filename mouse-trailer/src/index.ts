1f583f8cfd1b3fba44556825f336c60360467471
Baymurat <baymurat.orazov@mail.ru>
2022-11-04 16:52:03 +0500

    add icons to mouse trailer

--- mouse-trailer/src/index.ts
+++ mouse-trailer/src/index.ts
@@ -1,16 +1,32 @@
 import "../style/style.scss";
 
 const trailer = document.querySelector<HTMLDivElement>("#trailer");
+const trailerIcon = document.querySelector("#trailer-icon");
+
+const getInteractingIcon = (type: string): string => {
+  switch (type) {
+    case "video":
+      return "fa-solid fa-play";
+    default:
+      return "fa-solid fa-arrow-up-right-from-square";
+  }
+};
 
 const mouseMoveListener = (e: MouseEvent) => {
   const x = e.clientX - trailer.offsetWidth / 2;
   const y = e.clientY - trailer.offsetHeight / 2;
 
-  const interactable = (e.target as HTMLDivElement).closest(".interactable");
+  const interactable = (e.target as HTMLDivElement).closest<HTMLDivElement>(".interactable");
   const interacting = interactable !== null;
 
+  trailerIcon.className = "";
+
+  if (interacting) {
+    trailerIcon.className = getInteractingIcon(interactable.dataset.type);
+  }
+
   const keyframes: PropertyIndexedKeyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.5 : 1})`,
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 4 : 1})`,
   };
 
   trailer.animate(keyframes, {
